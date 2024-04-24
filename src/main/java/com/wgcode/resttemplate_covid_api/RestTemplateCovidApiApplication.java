a112ade2a2a26839c08a3c6b12a04230a8bd2c3a
gfizzle <winstongdouglas4@gmail.com>
2024-04-24 02:54:54 -0500

    first commit

---
+++ src/main/java/com/wgcode/resttemplate_covid_api/RestTemplateCovidApiApplication.java
@@ -0,0 +1,13 @@
+package com.wgcode.resttemplate_covid_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTemplateCovidApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestTemplateCovidApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wgcode/resttemplate_covid_api/RestTemplateCovidApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wgcode.resttemplate_covid_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestTemplateCovidApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
